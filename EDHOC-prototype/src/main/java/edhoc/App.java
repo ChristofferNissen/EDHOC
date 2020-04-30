61df7ee9b6696912348903595314cfcbcc3259af
Christoffer Nissen <christoffer.nissen@gmail.com>
2020-04-30 18:03:57 +0200

    added initial variables

--- EDHOC-prototype/src/main/java/edhoc/App.java
+++ EDHOC-prototype/src/main/java/edhoc/App.java
@@ -8,6 +8,23 @@ public class App
 {
     public static void main( String[] args )
     {
+
+        // Fixed parameters for our project
+        int method = 0;
+        int corr = 3;
+
+        // EDHOC parameters
+        int METHOD_CORR = 4 * method + corr; 
+
+
+        // Send out message one
+        
+
+        // send out message two
+
+        // send out message three
+
+
         System.out.println( "Hello World!" );
     }
 }
