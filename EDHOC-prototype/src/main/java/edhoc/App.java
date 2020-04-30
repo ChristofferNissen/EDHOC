a5681abc49d25eb089e291b77c04030e857eef09
rebn <rebn@itu.dk>
2020-04-30 15:57:25 +0200

    Added some structure

--- EDHOC-prototype/src/main/java/edhoc/App.java
+++ EDHOC-prototype/src/main/java/edhoc/App.java
@@ -8,6 +8,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+		Initiator initiator = new Initiator(0,3);
+		Responder responder = new Responder();
+		int message1 = initiator.createMessage1();
+		int message2 = responder.createMessage2(message1);
+		int message3 = initiator.createMessage3(message2);
+		boolean valid = responder.validateMessage3(message3);
+        System.out.println("Valid: " + valid);
     }
 }
